2ebb6d2440be391c2c8a7d1301a643245b49f678
Fabien De deken <fabien.dedeken@yahoo.fr>
2021-06-13 23:35:20 +0200

    Simplification du code, création de la classe Main

--- Project02Eclipse/src/com/hemebiotech/analytics/AnalyticsCounter.java
+++ Project02Eclipse/src/com/hemebiotech/analytics/AnalyticsCounter.java
@@ -4,41 +4,81 @@ import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.FileWriter;
 
+/**
+ * class reads a file of symptoms, counts in a map each symptom 
+ * and writes the result in a file
+ * 
+ * @author fabien
+ *
+ */
 public class AnalyticsCounter {
-	private static int headacheCount = 0; 
-	private static int rashCount = 0; 
-	private static int pupilCount = 0; 
-	
-	public static void main(String args[]) throws Exception {
-		
-		// correction: path of file source
-		String fileSymptom = "Project02Eclipse/symptoms.txt";
-		
-		// first get input
-		BufferedReader reader = new BufferedReader(new FileReader(fileSymptom));
-		String line = reader.readLine();
 
+	private static int headacheCount = 0;	
+	private static int rashCount = 0;		
+	private static int pupilCount = 0;		
+
+	/**
+	 * file path of source file 
+	 */
+	String fileSymptoms;
+
+	/**
+	 * constructor
+	 * @param fileSymptoms filepath a full or partial path to file with symptom 
+	 * strings in it, one per line
+	 */
+	AnalyticsCounter(String fileSymptoms) {
+		this.fileSymptoms = fileSymptoms;
+	}
+
+	/**
+	 * <p>reads source file line per line</p>
+	 * <p>counts occurrences for headache, rash and pupils</p>
+	 * <p><b>notice:</b> reading stops at the end of file then closes the file</p>
+	 * 
+	 * @throws Exception if source file doesn't exist
+	 */
+	public void reading() throws Exception {
+		BufferedReader reader = new BufferedReader (new FileReader(fileSymptoms));
+
+		/**
+		 * saves a symptom
+		 */
+		String line = reader.readLine();
+		
 		while (line != null) {
+
 			System.out.println("symptom from file: " + line);
 			if (line.equals("headache")) {
-				headacheCount++; // correction: headache counter
+				headacheCount++;
 				System.out.println("number of headaches: " + headacheCount);
-			} else if (line.equals("rash")) { // correction: symptom's name 
+			}
+			else if (line.equals("rash")) {
 				rashCount++;
-			} else if (line.contains("pupils")) {
+			}
+			else if (line.contains("pupils")) {
 				pupilCount++;
 			}
 
-			line = reader.readLine(); // get another symptom
+			line = reader.readLine();	// get another symptom
 		}
-		
-		reader.close(); // correction: close file source
-		
-		// correction: changes file's name by results.out
-		FileWriter writer = new FileWriter("results.out");
+
+		reader.close();
+	}
+
+	/**
+     * saves the name of each symptom and the result of the symptom's count 
+     * in a file
+     * 
+	 * @throws IOException if error in file name
+     * 
+     */
+	public void saving() throws Exception {
+		FileWriter writer = new FileWriter ("results.out");
 		writer.write("headache: " + headacheCount + "\n");
 		writer.write("rash: " + rashCount + "\n");
 		writer.write("dialated pupils: " + pupilCount + "\n");
 		writer.close();
 	}
+
 }
---
+++ Project02Eclipse/src/com/hemebiotech/analytics/Main.java
@@ -0,0 +1,24 @@
+package com.hemebiotech.analytics;
+
+public class Main {
+
+	/**
+	 * <p>simple main creates an AnalyticsCounter object</p>
+	 * <p>fileSymptoms is a full or partial path to file with symptom 
+	 * strings in it, one per line</p>
+	 * 
+	 * @throws Exception if source file doesn't exist
+	 * 
+	 * @see AnalyticsCounter#AnalyticsCounter(String)
+	 * @see AnalyticsCounter#reading()
+	 * @see AnalyticsCounter#saving()
+	 */
+	public static void main(String[] args) throws Exception {
+
+		String fileSymptoms = "Project02Eclipse/symptoms.txt";
+		AnalyticsCounter analyticsCounter = new AnalyticsCounter(fileSymptoms);
+		analyticsCounter.reading();
+		analyticsCounter.saving();
+	}
+
+} 
