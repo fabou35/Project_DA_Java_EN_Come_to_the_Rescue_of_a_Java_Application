a8fb28ce887b4bfaa7d955a6f1b0fa1a0db67062
Fabien De deken <fabien.dedeken@yahoo.fr>
2021-06-13 23:18:43 +0200

    Correction des erreurs d'Alex

--- Project02Eclipse/src/com/hemebiotech/analytics/AnalyticsCounter.java
+++ Project02Eclipse/src/com/hemebiotech/analytics/AnalyticsCounter.java
@@ -5,36 +5,37 @@ import java.io.FileReader;
 import java.io.FileWriter;
 
 public class AnalyticsCounter {
-	private static int headacheCount = 0;	// initialize to 0
-	private static int rashCount = 0;		// initialize to 0
-	private static int pupilCount = 0;		// initialize to 0
+	private static int headacheCount = 0; 
+	private static int rashCount = 0; 
+	private static int pupilCount = 0; 
 	
 	public static void main(String args[]) throws Exception {
+		
+		// correction: path of file source
+		String fileSymptom = "Project02Eclipse/symptoms.txt";
+		
 		// first get input
-		BufferedReader reader = new BufferedReader (new FileReader("symptoms.txt"));
+		BufferedReader reader = new BufferedReader(new FileReader(fileSymptom));
 		String line = reader.readLine();
 
-		int i = 0;	// set i to 0
-		int headCount = 0;	// counts headaches
 		while (line != null) {
-			i++;	// increment i
 			System.out.println("symptom from file: " + line);
 			if (line.equals("headache")) {
-				headCount++;
-				System.out.println("number of headaches: " + headCount);
-			}
-			else if (line.equals("rush")) {
+				headacheCount++; // correction: headache counter
+				System.out.println("number of headaches: " + headacheCount);
+			} else if (line.equals("rash")) { // correction: symptom's name 
 				rashCount++;
-			}
-			else if (line.contains("pupils")) {
+			} else if (line.contains("pupils")) {
 				pupilCount++;
 			}
 
-			line = reader.readLine();	// get another symptom
+			line = reader.readLine(); // get another symptom
 		}
 		
-		// next generate output
-		FileWriter writer = new FileWriter ("result.out");
+		reader.close(); // correction: close file source
+		
+		// correction: changes file's name by results.out
+		FileWriter writer = new FileWriter("results.out");
 		writer.write("headache: " + headacheCount + "\n");
 		writer.write("rash: " + rashCount + "\n");
 		writer.write("dialated pupils: " + pupilCount + "\n");
